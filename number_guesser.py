24095faadc6b93356425d04edebbb09b8ff36fb1
Rakshith-Sudan <raksud15oct@gmail.com>
2022-06-22 12:32:54 +0530

    f

--- number_guesser.py
+++ number_guesser.py
@@ -12,14 +12,26 @@ else:
     quit()
 
 random_number = random.randint(0,top_of_range)
+guesses = 0
 
 while True:
-    
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it right!")
+        break
+
+    elif user_guess > random_number:
+        print("You are above the number!")
+
+    else:
+        print("You are below the number!")
 
 
-
-
-
-
-print(random_number)
-
+print("You got it in", guesses, "guesses!")     
